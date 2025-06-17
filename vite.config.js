800659e83731f055e236fa0688ee77ea6b3ebf7f
Deepika . L <146464527+DeepikaLeelakumar@users.noreply.github.com>
2025-06-17 13:55:38 +0530

    Fix: Corrected css path for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ Use './' for Netlify (relative path)
+  base: './',
   plugins: [react()],
 })
